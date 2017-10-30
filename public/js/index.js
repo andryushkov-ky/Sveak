c9529b85767481e64066c7324a70b64543b38a2c
Andryushkov-ky <ka@madrobots.ru>
2017-10-30 12:07:35 +0700

    [master] prepared to build app

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import '../css/index.pcss';
+
+const App = () => {
+    return (
+        <div>Hello, My Little Baby Girl!</div>
+    )
+};
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
