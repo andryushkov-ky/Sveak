e9b04a87bbdf733cb79c02822e2b1992aa0e39c7
Andryushkov-ky <ka@madrobots.ru>
2017-10-30 12:45:08 +0700

    [master] configured routing

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
 import '../css/index.pcss';
 
-const App = () => {
-    return (
-        <div>Hello, My Little Baby Girl!</div>
-    )
-};
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {Router, Route} from 'react-router'
+
+import configureStore from 'store'
+import ClientsList from 'containers/clientsList'
+
+const store = configureStore();
+
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path='/' component={ClientsList} />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
